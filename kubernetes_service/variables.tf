9df3f368c0ec4a07f0d239fc8b7bd644cf415a0b
manimike00 <manimike00@gmail.com>
2024-07-07 17:52:50 +0000

    add default values

--- kubernetes_service/variables.tf
+++ kubernetes_service/variables.tf
@@ -11,9 +11,3 @@ variable "project" {
 variable "location" {
   default = "centralindia"
 }
-
-## virtual network
-#variable "address_space" {}
-#variable "address_prefixes_default_np" {}
-#variable "address_prefixes_monitoring_np" {}
-#variable "address_prefixes_appgw" {}
